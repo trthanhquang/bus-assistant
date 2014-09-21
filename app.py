5b462469352f111c1ef4a1f47f46822699d41773
Louis <trthanhquang@gmail.com>
2014-09-21 16:55:03 -0700

    integrating with Foundation

--- app.py
+++ app.py
@@ -11,20 +11,45 @@ import tornado.gen
 from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
 
-class IndexHandler(tornado.web.RequestHandler):
-    def initialize(self):
-    	pass
+class busTime:
+    def __init__(self,busCode,busTime):
+        self.busCode = busCode #String
+        self.busTime = busTime #List of String
+    
+    def __str__(self):
+        return self.busCode
 
+class TimeHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        pass
+    
     def get(self):
-    	data = time.ctime(time.time())
-        self.render("index.html", data=data)
+        data = time.ctime(time.time())
+        self.render("time.html", data=data)
+
+class BusTimingHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        pass
+
+    def get(self, bus_id = "16367"):
+        busses = []
+        if (bus_id == "16367"):
+            busses = [
+                busTime('8AX',['2','10']), 
+                busTime('8X',['1','10','30']),
+                busTime('9',['2','18','47']),
+            ]
+        self.render("index.html", busStop = bus_id, busses=busses)
 
 class Application(tornado.web.Application):
     def __init__(self):
 
         handlers = [
-            (r"/", IndexHandler),
+            (r"/",BusTimingHandler),
+            (r"/time", TimeHandler),
+            (r"/bus/([0-9]+)",BusTimingHandler)
         ]
+
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__),"static"),
--- static/scripts/timing.js
+++ static/scripts/timing.js
@@ -2,3 +2,12 @@ function updateTime(){
     document.getElementById('time').innerHTML = new Date();
     var t = setTimeout(function(){updateTime()},500)
 }
+
+var txt = ""
+function setText(val){
+	txt = val
+}
+
+function updateText(){
+    document.getElementById('txt').innerHTML = txt;
+}
